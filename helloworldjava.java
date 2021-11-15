396d131eaa9f98b334b213025e1dfc5799ad911e
burlapriyu <93882085+burlapriyu@users.noreply.github.com>
2021-11-15 21:00:01 +0530

    Update helloworldjava.java

--- helloworldjava.java
+++ helloworldjava.java
@@ -1,6 +1,6 @@
 public class helloworldjava {
    public static void main(String[] args) {
-    System.out.println("Hello World from DevOps! hi");
+    System.out.println("Hello World from DevOps! hi dev");
     }
 }
 
